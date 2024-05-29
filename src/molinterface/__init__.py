13d78d622b58608644eb68a6c69f6e17bd6ab353
Elie Abou Jaoudé <160876323+EAJ7@users.noreply.github.com>
2024-05-29 17:59:36 +0200

    Update __init__.py

--- src/molinterface/__init__.py
+++ src/molinterface/__init__.py
@@ -1,20 +1,5 @@
 """Interface that gives useful infos, stereochemistry and chemical groups from a compound's name in the clipboard."""
 
-from .MOLInterface import (
-  smarts_patterns, 
-  find_smiles_patterns, 
-  find_rings, 
-  format_rings, 
-  open_url, 
-  find_chemical_groups, 
-  get_compound_info, 
-  display_image1_in_tkinter, 
-  display_image2_in_tkinter, 
-  highlight_chemical_groups, 
-  update_gui_labels, 
-  update_molecule_info, 
-  clipboard_monitor, 
-  toggle_image_display, 
-  main
-)
+from .MOLInterface import smarts_patterns, find_smiles_patterns, find_rings, format_rings, open_url, find_chemical_groups, get_compound_info, display_image1_in_tkinter, display_image2_in_tkinter, highlight_chemical_groups, update_gui_labels, update_molecule_info, clipboard_monitor, toggle_image_display, main
+
 __version__ = "0.0.1"
